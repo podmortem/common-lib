603aa62ad3dad996284f3fe8395d78c5f79a4f5e
Caleb Evans <caevans@redhat.com>
2025-06-23 20:59:18 +0000

    patch(analysis metadata): refine the analysis metadata

--- src/main/java/com/redhat/podmortem/common/model/analysis/AnalysisMetadata.java
+++ src/main/java/com/redhat/podmortem/common/model/analysis/AnalysisMetadata.java
@@ -1,12 +1,28 @@
 package com.redhat.podmortem.common.model.analysis;
 
-import io.quarkus.runtime.annotations.RegisterForReflection;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
 
-@RegisterForReflection
 public class AnalysisMetadata {
+    @JsonProperty("analyzed_at")
+    private String analyzedAt;
+
+    @JsonProperty("total_lines")
     private int totalLines;
-    private int significantEvents;
+
+    @JsonProperty("processing_time_ms")
     private long processingTimeMs;
+
+    @JsonProperty("patterns_used")
+    private List<String> patternsUsed;
+
+    public String getAnalyzedAt() {
+        return analyzedAt;
+    }
+
+    public void setAnalyzedAt(String analyzedAt) {
+        this.analyzedAt = analyzedAt;
+    }
 
     public int getTotalLines() {
         return totalLines;
@@ -16,19 +32,19 @@ public class AnalysisMetadata {
         this.totalLines = totalLines;
     }
 
-    public int getSignificantEvents() {
-        return significantEvents;
-    }
-
-    public void setSignificantEvents(int significantEvents) {
-        this.significantEvents = significantEvents;
-    }
-
     public long getProcessingTimeMs() {
         return processingTimeMs;
     }
 
     public void setProcessingTimeMs(long processingTimeMs) {
         this.processingTimeMs = processingTimeMs;
+    }
+
+    public List<String> getPatternsUsed() {
+        return patternsUsed;
+    }
+
+    public void setPatternsUsed(List<String> patternsUsed) {
+        this.patternsUsed = patternsUsed;
     }
 }
