f3f91237ec32c33c05dec28fa9f67b679f152abc
Caleb Evans <caevans@redhat.com>
2025-06-23 21:09:14 +0000

    patch(analysis): include analysis summary in the analysis result

--- src/main/java/com/redhat/podmortem/common/model/analysis/AnalysisResult.java
+++ src/main/java/com/redhat/podmortem/common/model/analysis/AnalysisResult.java
@@ -1,12 +1,23 @@
 package com.redhat.podmortem.common.model.analysis;
 
-import io.quarkus.runtime.annotations.RegisterForReflection;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import java.util.List;
 
-@RegisterForReflection
 public class AnalysisResult {
+    @JsonProperty("analysis_id")
+    private String analysisId;
+
     private AnalysisMetadata metadata;
+    private AnalysisSummary summary;
     private List<MatchedEvent> events;
+
+    public String getAnalysisId() {
+        return analysisId;
+    }
+
+    public void setAnalysisId(String analysisId) {
+        this.analysisId = analysisId;
+    }
 
     public AnalysisMetadata getMetadata() {
         return metadata;
@@ -14,6 +25,14 @@ public class AnalysisResult {
 
     public void setMetadata(AnalysisMetadata metadata) {
         this.metadata = metadata;
+    }
+
+    public AnalysisSummary getSummary() {
+        return summary;
+    }
+
+    public void setSummary(AnalysisSummary summary) {
+        this.summary = summary;
     }
 
     public List<MatchedEvent> getEvents() {
