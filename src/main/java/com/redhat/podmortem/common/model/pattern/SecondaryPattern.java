078e906b4d66f38e97b43cc0e9fa8dc807ac5d52
Caleb Evans <caevans@redhat.com>
2025-06-23 20:33:21 +0000

    feat(pattern): add get/set compiled regex to secondary patterns

--- src/main/java/com/redhat/podmortem/common/model/pattern/SecondaryPattern.java
+++ src/main/java/com/redhat/podmortem/common/model/pattern/SecondaryPattern.java
@@ -1,5 +1,6 @@
 package com.redhat.podmortem.common.model.pattern;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 
@@ -10,6 +11,8 @@ public class SecondaryPattern {
 
     @JsonProperty("proximity_window")
     private int proximityWindow;
+
+    @JsonIgnore private java.util.regex.Pattern compiledRegex;
 
     public String getRegex() {
         return regex;
@@ -33,5 +36,15 @@ public class SecondaryPattern {
 
     public void setProximityWindow(int proximityWindow) {
         this.proximityWindow = proximityWindow;
+    }
+
+    @JsonIgnore
+    public java.util.regex.Pattern getCompiledRegex() {
+        return compiledRegex;
+    }
+
+    @JsonIgnore
+    public void setCompiledRegex(java.util.regex.Pattern compiledRegex) {
+        this.compiledRegex = compiledRegex;
     }
 }
