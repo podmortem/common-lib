9a1c6c667ec2ebaf4c3c83f12fc44325050da09e
Caleb Evans <caevans@redhat.com>
2025-06-23 21:05:27 +0000

    patch(analysis): add an analysis sumamry object

---
+++ src/main/java/com/redhat/podmortem/common/model/analysis/AnalysisSummary.java
@@ -0,0 +1,39 @@
+package com.redhat.podmortem.common.model.analysis;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.Map;
+
+public class AnalysisSummary {
+    @JsonProperty("significant_events")
+    private int significantEvents;
+
+    @JsonProperty("highest_severity")
+    private String highestSeverity;
+
+    @JsonProperty("severity_distribution")
+    private Map<String, Long> severityDistribution;
+
+    public int getSignificantEvents() {
+        return significantEvents;
+    }
+
+    public void setSignificantEvents(int significantEvents) {
+        this.significantEvents = significantEvents;
+    }
+
+    public String getHighestSeverity() {
+        return highestSeverity;
+    }
+
+    public void setHighestSeverity(String highestSeverity) {
+        this.highestSeverity = highestSeverity;
+    }
+
+    public Map<String, Long> getSeverityDistribution() {
+        return severityDistribution;
+    }
+
+    public void setSeverityDistribution(Map<String, Long> severityDistribution) {
+        this.severityDistribution = severityDistribution;
+    }
+}
