35533c74beb48b6f493d12b3fe9a6671149b820c
Caleb Evans <caevans@redhat.com>
2025-06-25 16:01:30 +0000

    patch(analysis): add anaylsis request model

---
+++ src/main/java/com/redhat/podmortem/common/model/analysis/AnalysisRequest.java
@@ -0,0 +1,40 @@
+package com.redhat.podmortem.common.model.analysis;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.redhat.podmortem.common.model.provider.AIProviderConfig;
+
+/**
+ * Request model for AI analysis containing both the log analysis results and the AI provider
+ * configuration sent by the operator.
+ */
+public class AnalysisRequest {
+
+    @JsonProperty("analysis_result")
+    private AnalysisResult analysisResult;
+
+    @JsonProperty("provider_config")
+    private AIProviderConfig providerConfig;
+
+    public AnalysisRequest() {}
+
+    public AnalysisRequest(AnalysisResult analysisResult, AIProviderConfig providerConfig) {
+        this.analysisResult = analysisResult;
+        this.providerConfig = providerConfig;
+    }
+
+    public AnalysisResult getAnalysisResult() {
+        return analysisResult;
+    }
+
+    public void setAnalysisResult(AnalysisResult analysisResult) {
+        this.analysisResult = analysisResult;
+    }
+
+    public AIProviderConfig getProviderConfig() {
+        return providerConfig;
+    }
+
+    public void setProviderConfig(AIProviderConfig providerConfig) {
+        this.providerConfig = providerConfig;
+    }
+}
