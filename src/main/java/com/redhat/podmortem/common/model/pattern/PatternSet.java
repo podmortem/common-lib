1bbf0840555b257374892493cb78698b2b916fbb
Caleb Evans <caevans@redhat.com>
2025-07-17 17:23:24 +0000

    fix(patterns): add related patterns

--- src/main/java/com/redhat/podmortem/common/model/pattern/PatternSet.java
+++ src/main/java/com/redhat/podmortem/common/model/pattern/PatternSet.java
@@ -1,13 +1,18 @@
 package com.redhat.podmortem.common.model.pattern;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.quarkus.runtime.annotations.RegisterForReflection;
 import java.util.List;
+import java.util.Map;
 
 @RegisterForReflection
 public class PatternSet {
     private Metadata metadata;
     private List<String> categories;
     private List<Pattern> patterns;
+
+    @JsonProperty("related_patterns")
+    private List<Map<String, Object>> relatedPatterns;
 
     public Metadata getMetadata() {
         return metadata;
@@ -31,5 +36,13 @@ public class PatternSet {
 
     public void setPatterns(List<Pattern> patterns) {
         this.patterns = patterns;
+    }
+
+    public List<Map<String, Object>> getRelatedPatterns() {
+        return relatedPatterns;
+    }
+
+    public void setRelatedPatterns(List<Map<String, Object>> relatedPatterns) {
+        this.relatedPatterns = relatedPatterns;
     }
 }
